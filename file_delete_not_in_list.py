5a59877395b30089e56f1679f8bc2445b8edb974
Jurgen van der Ploeg <ploegj@gmail.com>
2020-11-15 15:43:16 +0100

    first commit

--- file_delete_not_in_list.py
+++ file_delete_not_in_list.py
@@ -1,8 +1,14 @@
 import os
+# this script will delete all files which are not listed in str_files_to_keep from str_path
+
 set_device = set()
 
+# path containing files to delete/keep
 str_path = 'data'
-with open("files_to_keep.txt", "r") as fh:
+str_files_to_keep = 'files_to_keep.txt'
+
+# read files to keep into set_device set, reading them into a set keeps them unique
+with open(str_files_to_keep, "r") as fh:
     line = fh.readline()
     while line != '':
         # add first 6 characters of devicename to set
@@ -10,7 +16,8 @@ with open("files_to_keep.txt", "r") as fh:
         # print(line.strip())
         line = fh.readline()
 
-for filename in os.listdir('data'):
+# loop through all files in str_path and delete the filenames that cannot be found in set_device
+for filename in os.listdir(str_path):
     if filename[:6] not in set_device:
         print(str_path + '\\' + filename)
         os.unlink(str_path + '\\' + filename)
